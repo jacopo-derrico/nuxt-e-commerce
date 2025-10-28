91df793c2ebd22e5ea6519657c753740ecc29401
jacopo-derrico <derricoj.design@gmail.com>
2025-10-28 10:00:53 +0100

    feat: add footer

--- app/app.vue
+++ app/app.vue
@@ -41,6 +41,41 @@
     <UMain>
       <NuxtPage />
     </UMain>
+
+    <UFooter>
+      <template #left>
+        <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }}</p>
+      </template>
+
+      <UNavigationMenu :items="footerItems" variant="link" />
+
+      <template #right>
+        <UButton
+          icon="i-simple-icons-discord"
+          color="neutral"
+          variant="ghost"
+          to="https://go.nuxt.com/discord"
+          target="_blank"
+          aria-label="Discord"
+        />
+        <UButton
+          icon="i-simple-icons-x"
+          color="neutral"
+          variant="ghost"
+          to="https://go.nuxt.com/x"
+          target="_blank"
+          aria-label="X"
+        />
+        <UButton
+          icon="i-simple-icons-github"
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/nuxt/nuxt"
+          target="_blank"
+          aria-label="GitHub"
+        />
+      </template>
+    </UFooter>
   </UApp>
 </template>
 
@@ -76,4 +111,21 @@ import { chip } from '#build/ui';
 
     return crumbs;
   });
+
+  const footerItems: NavigationMenuItem[] = [
+  {
+    label: 'Figma Kit',
+    to: 'https://go.nuxt.com/figma-ui'
+  },
+  {
+    label: 'Playground',
+    to: 'https://stackblitz.com/edit/nuxt-ui',
+    target: '_blank'
+  },
+  {
+    label: 'Releases',
+    to: 'https://github.com/nuxt/ui/releases',
+    target: '_blank'
+  }
+]
 </script>
